dcf1c786fde0fa7ac72a26fe86f304b5187ee149
digwendal <gwendal.didot@irisa.fr>
2018-08-22 14:35:01 +0200

    improve(ts): Adding customisation for the TextToSpeech command.

--- malai-core/org.malai.ts/src/src-core/command/library/TextToSpeech.ts
+++ malai-core/org.malai.ts/src/src-core/command/library/TextToSpeech.ts
@@ -13,11 +13,38 @@ import {CommandImpl} from "../CommandImpl";
 
 export class TextToSpeech extends CommandImpl {
 
-    private textToSpeech: string;
+    private readonly speechSynth: SpeechSynthesis;
+    private readonly speechSynthUtterance: SpeechSynthesisUtterance;
 
-    public constructor(data: string) {
+    private readonly textToSpeech: string;
+    private readonly pitch: number;
+    private readonly rate: number;
+    private readonly lang: string;
+    private readonly voice: SpeechSynthesisVoice;
+    private readonly volume: number;
+
+    /**
+     *
+     * @param {string} data
+     * @param {number} [pitch=0] The value of utter.pitch
+     * @param {number} [rate=0] The value of utter.rate
+     * @param {string} [lang="en-UK"] The lang use by the utter
+     * @param {SpeechSynthesisVoice} [voice] The voice use by the utter, if not provide it will picked the voice suit for the utter.lang
+     * @param {number} [volume=1] The volume of the utter will speak
+     */
+
+    public constructor(data: string, pitch ?: number, rate ?: number, lang ?: string, voice ?: SpeechSynthesisVoice, volume ?: number) {
         super();
+        this.speechSynth = new SpeechSynthesis();
+        this.speechSynthUtterance = new SpeechSynthesisUtterance();
         this.textToSpeech = data;
+        this.pitch = pitch ? pitch : 0;
+        this.rate = rate ? rate : 0;
+        this.lang = lang ? lang : "en-UK";
+        if (voice !== undefined) {
+            this.voice = voice;
+        }
+        this.volume = volume ? volume : 1;
     }
 
     public canDo(): boolean {
@@ -25,8 +52,14 @@ export class TextToSpeech extends CommandImpl {
     }
 
     protected doCmdBody(): void {
-        const synth = window.speechSynthesis;
-        const utter = new SpeechSynthesisUtterance(this.textToSpeech);
+        const synth = this.speechSynth;
+        const utter = this.speechSynthUtterance;
+        utter.text = this.textToSpeech;
+        utter.pitch = this.pitch;
+        utter.rate = this.rate;
+        utter.lang = this.lang;
+        utter.voice = this.voice;
+        utter.volume = this.volume;
         synth.speak(utter);
     }
 }
